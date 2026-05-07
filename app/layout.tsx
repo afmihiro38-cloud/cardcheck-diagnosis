61ad4d08880e860404658acddb921c44ca2b7c76
afmihiro38-cloud <af.mihiro38@gmail.com>
2026-05-07 21:59:41 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,13 @@ export default function RootLayout({
             });
           `}
         </Script>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wne1ekkcia");
+</script>
       </body>
     </html>
   );
